69e6126b8490f2360e3d86bd9dfc680a7aa25d4e
Andreas Lind <andreas@one.com>
2015-05-10 07:52:38 +0200

    Fixed jshint complaints.

--- generate-site.js
+++ generate-site.js
@@ -1,10 +1,8 @@
-/*global messy*/
+/*global messy:true*/
 var argv = require('minimist')(process.argv.slice(2));
 
 messy = require('messy');
-unexpected = require('unexpected').clone().installPlugin(require('./lib/unexpectedMessy'));
-
-unexpected.installPlugin(require('./lib/unexpectedMessy'));
+var unexpected = require('unexpected').clone().installPlugin(require('./lib/unexpectedMessy'));
 var generator = require('unexpected-documentation-site-generator');
 argv.unexpected = unexpected;
 generator(argv);
