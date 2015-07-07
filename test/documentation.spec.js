fca268995f10dbaf55ae8555d3bd4563b45dcf8e
Andreas Lind <andreas@one.com>
2015-07-07 13:46:47 +0200

    Always use magicpen-media to render video/*, image/*, and audio/* bodies, not only in HTML mode.

--- test/documentation.spec.js
+++ test/documentation.spec.js
@@ -104,7 +104,7 @@ describe("documentation tests", function () {
                 "\n" +
                 "Content-Type: image/png\n" +
                 "\n" +
-                "Buffer([0xFF, 0xD8, 0xFF, 0xE0, 0x00, 0x10, 0x4A, 0x46, 0x49, 0x46, 0x00, 0x01, 0x01, 0x00, 0x00, 0x01 /* 13493 more */ ])\n" +
+                "Buffer[13509] (image/png)\n" +
                 "to satisfy { headers: { 'Content-Type': 'image/gif' } }\n" +
                 "\n" +
                 "\n" +
@@ -112,7 +112,7 @@ describe("documentation tests", function () {
                 "                        // -image/png\n" +
                 "                        // +image/gif\n" +
                 "\n" +
-                "Buffer([0xFF, 0xD8, 0xFF, 0xE0, 0x00, 0x10, 0x4A, 0x46, 0x49, 0x46, 0x00, 0x01, 0x01, 0x00, 0x00, 0x01 /* 13493 more */ ])"
+                "Buffer[13509] (image/png)"
             );
         }
         return expect.promise.all(testPromises);
