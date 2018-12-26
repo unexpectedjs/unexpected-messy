909e0419ac7a4fa36d56610cd6fe8bc1aa88c935
Alex J Burke <alex@alexjeffburke.com>
2018-12-26 18:44:03 +0100

    Issue a clone() in the unexpected-markdown bootstrap script.

--- bootstrap-unexpected-markdown.js
+++ bootstrap-unexpected-markdown.js
@@ -1,6 +1,6 @@
 /* global messy:true, unexpected:true */
 /* exported messy, unexpected */
 messy = require('messy');
-unexpected = require('unexpected');
+unexpected = require('unexpected').clone();
 unexpected.output.preferredWidth = 80;
 unexpected.use(require('./lib/unexpectedMessy'));
