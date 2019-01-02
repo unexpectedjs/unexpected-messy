d81346dfcff7f7a848fe06e4ea25d05975bd043d
Andreas Lind <andreaslindpetersen@gmail.com>
2019-01-03 00:40:55 +0100

    eslint --fix .

--- generate-site.js
+++ generate-site.js
@@ -4,7 +4,9 @@ var argv = require('minimist')(process.argv.slice(2));
 
 global.require = require;
 messy = require('messy');
-var unexpected = require('unexpected').clone().installPlugin(require('./lib/unexpectedMessy'));
+var unexpected = require('unexpected')
+  .clone()
+  .installPlugin(require('./lib/unexpectedMessy'));
 var generator = require('unexpected-documentation-site-generator');
 unexpected.output.preferredWidth = 80;
 argv.unexpected = unexpected;
